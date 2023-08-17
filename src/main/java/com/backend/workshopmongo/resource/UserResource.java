f7a51fb81cc8a9f65e5161c5e0cb0f6d207937c3
Gabriel nascimento <gabrielnasciment123@hotmail.com>
2023-08-17 15:56:54 -0300

    adicionando um objeto que melhor nos traz as respostas em http e o testamos no postman

--- src/main/java/com/backend/workshopmongo/resource/UserResource.java
+++ src/main/java/com/backend/workshopmongo/resource/UserResource.java
@@ -4,32 +4,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.workshopmongo.domain.User;
 
-@RestController //diz que a nossa classe é um recurso rest
-@RequestMapping(value = "/users") //aqui damos o caminho do endpoint
+@RestController // diz que a nossa classe é um recurso rest
+@RequestMapping(value = "/users") // aqui damos o caminho do endpoint
 public class UserResource {
-	
-	@RequestMapping(method=RequestMethod.GET) //o metodo get é para obter informações no metodo rest O GETMAPPING
-	//funciona igual
-	public List<User> findAll(){
+
+	@RequestMapping(method = RequestMethod.GET) // o metodo get é para obter informações no metodo rest O GETMAPPING
+	// funciona igual
+
+	/*
+	 * public List<User> findAll(){
+	 * 
+	 * //fazendo um teste para saber se estamos //instanciando o objeto corretamente
+	 * com seus atributos
+	 * 
+	 * User maria = new User("1", "Maria Brown", "mariabrown@gmail.com"); User jones
+	 * = new User("2", "Jones Green", "jonesgreen@gmail.com"); List<User> list = new
+	 * ArrayList<>(); //definindo o obj da lista
+	 * 
+	 * list.addAll(Arrays.asList(maria, jones)); //adicionando esses valores de
+	 * teste direto //no nosso objeto da list "List<User> list
+	 * 
+	 * return list;
+	 * 
+	 */ //isso é um retorno de lista mto basico
+
+	public ResponseEntity<List<User>>  findAll() {
+		/* aqui usaremos um obj mais sofisticado no java , ele vai encapsular
+		 * toda uma estrutura necessaria para a gente retornar respostas HTTP com cabeçalho
+		 * possiveis erros e td mais */
 		
-		//fazendo um teste para saber se estamos 
-		//instanciando o objeto corretamente com seus atributos
 		
+
+		// fazendo um teste para saber se estamos
+		// instanciando o objeto corretamente com seus atributos
+
 		User maria = new User("1", "Maria Brown", "mariabrown@gmail.com");
 		User jones = new User("2", "Jones Green", "jonesgreen@gmail.com");
-		List<User> list = new ArrayList<>(); //definindo o obj da lista
-		
-		list.addAll(Arrays.asList(maria, jones)); //adicionando esses valores de teste direto
-		//no nosso objeto da list "List<User> list
-		
-		return list;
-		
+		List<User> list = new ArrayList<>(); // definindo o obj da lista
+
+		list.addAll(Arrays.asList(maria, jones)); // adicionando esses valores de teste direto
+		// no nosso objeto da list "List<User> list
+
+		return ResponseEntity.ok().body(list);
+		/* o "ok" vai instanciar o nosso ResponseEntity com o codigo de resposta http que a resposta
+		 * ocorreu corretamente, e o body vai ser o corpo da resposta*/
+
 	}
 
 }
